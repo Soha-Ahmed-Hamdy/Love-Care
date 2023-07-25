ec597989bc05a9b3bba28b82226bd529a6333121
Soha Ahmed Hamdy <soha7hmed@gmail.com>
2023-07-25 17:29:00 +0300

    Initial Commit

---
+++ Love&Care/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Love&Care
+//
+//  Created by Soha Ahmed Hamdy on 25/07/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Love&Care/Love_CareApp.swift
@@ -0,0 +1,17 @@
+//
+//  Love_CareApp.swift
+//  Love&Care
+//
+//  Created by Soha Ahmed Hamdy on 25/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct Love_CareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
